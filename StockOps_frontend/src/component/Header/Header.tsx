100eb7443d4a10afcf2d5ba0f982bd7975848415
AxelPONCET <axelponcet25000@gmail.com>
2025-05-02 17:41:39 +0200

    Feat : dynamisation du menu burger

--- StockOps_frontend/src/component/Header/Header.tsx
+++ StockOps_frontend/src/component/Header/Header.tsx
@@ -1,31 +1,49 @@
+import { useState } from "react";
 import { OutlineKeyboardArrowDown } from "../icon/arrowIcon";
 import "./Header.scss";
 
 function Header() {
+  const [menuIsOpen, setMenuIsOpen] = useState(false);
+
+  // Fonction pour ouvrir le menu burger
+  const handleOpenBurgerMenu = () => {
+    setMenuIsOpen((prev) => !prev);
+  };
+
   return (
-    <header className="header">
-      {/* Titre principale */}
-      <div className="titleSide">
-        <h1 className="headerTitle">StockOps</h1>
-      </div>
-      {/* Navigation caché en format mobile dépliable avec le burgerMenuButton */}
-      <div className="navSide">
-        <nav className="headerNav">
-          <ul className="headerNavList">
-            <li className="headerNavList__item">Dashboard</li>
-            <li className="headerNavList__item">Login</li>
-            <li className="headerNavList__item">Register</li>
-          </ul>
-        </nav>
-      </div>
-      {/* Boutton du menu burger visible seulement en format mobile */}
-      <div className="burgerMenuButton">
-        <div className="separator" />
-        <div className="arrow">
-          <OutlineKeyboardArrowDown />
+    <>
+      <header className="header">
+        {/* Titre principale */}
+        <div className="titleSide">
+          <h1 className="headerTitle">StockOps</h1>
         </div>
-      </div>
-    </header>
+        {/* Boutton du menu burger visible seulement en format mobile */}
+        <button
+          className="burgerMenuButton"
+          type="button"
+          onClick={handleOpenBurgerMenu}
+        >
+          <div className="separator" />
+          <div className="arrow">
+            <OutlineKeyboardArrowDown />
+          </div>
+        </button>
+      </header>
+       {/* Navigation caché en format mobile dépliable avec le burgerMenuButton */}
+       {menuIsOpen ? (
+          <div className="navSide">
+            <nav className="headerNav">
+              <ul className="headerNavList">
+                <li className="headerNavList__item">Dashboard</li>
+                <li className="headerNavList__item">Login</li>
+                <li className="headerNavList__item">Register</li>
+              </ul>
+            </nav>
+          </div>
+        ) : (
+          ""
+        )}
+    </>
   );
 }
 
